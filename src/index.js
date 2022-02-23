5939ae4abbac10c63e0b6adddd983371da1b5d8f
AndreiArtsiomenko <andartsiomenko@gmail.com>
2022-02-23 06:40:22 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let arr = n.toString().split('');
+    if (n < 0) return +(arr.slice(1).reverse().join(''));
+    return +(arr.reverse().join(''));
 }
